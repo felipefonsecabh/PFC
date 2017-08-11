275abdf4c82ba6d0ae46ae5c1eb74e398f2b9602
Felipe Fonseca <felipefonsecaufmg@outlook.com>
2017-08-11 00:01:43 -0300

    Interface da tela de operação completa. Falta apenas analisar o porquê do slider não estar responsivo para telas pequenas. Próximo passo é gerar os javascript de atualização da tela de acordo com dados de banco

--- WebSite/operation/views.py
+++ WebSite/operation/views.py
@@ -5,6 +5,7 @@ from .models import DataDisplay,Registers
 # Create your views here.
 def main(request):
     dp_col = DataDisplay.objects.all()
+    print(dp_col[0].name)
     reg = Registers.objects.latest('pk')
     context = {
         'dp_col': dp_col,
